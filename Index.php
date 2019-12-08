57dd5f34851aa56e003bcc68e228d8a687db4d3e
Jgomez11 <37757272+Jgomez11@users.noreply.github.com>
2019-12-08 10:30:42 -0600

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="JairoG">
+
+
+	<title>Smulador de Credito</title>
+
+	<!-- Additional CSS Files -->
+	<link rel="stylesheet" type="text/css" href="Frameworks/Bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="Recursos/Estilos/font-awesome.css">
+	<link rel="stylesheet" href="Recursos/Estilos/style.css">
+
+</head>
+
+<body>
+
+
+
+	<!-- ***** Header Area Start ***** -->
+	<header class="header-area header-sticky">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<nav class="main-nav">
+						<!-- ***** Logo Start ***** -->
+						<a href="Index.php" class="logo">
+							<img src="Recursos/Imagenes/logo.png" alt="SimuladorDeCredito" width="270px;" />
+						</a>
+						<!-- ***** Logo End ***** -->
+						<!-- ***** Menu Start ***** -->
+						<ul class="nav">
+							<li><a href="#">Tipos De Intereses </a></li>
+							<li><a href="#">Soporte </a></li>
+						</ul>
+
+						<!-- ***** Menu End ***** -->
+					</nav>
+				</div>
+			</div>
+		</div>
+	</header>
+	<!-- ***** Header Area End ***** -->
+
+	<!-- ***** Welcome Area Start ***** -->
+	<div class="welcome-area" id="welcome">
+
+		<!-- ***** Header Text Start ***** -->
+		<div class="header-text">
+			<div class="container">
+				<div class="row">
+					<div class="offset-xl-3 col-xl-6 offset-lg-2 col-lg-8 col-md-12 col-sm-12">
+						<h1><strong>  ¿Que Desea Simular?</strong></h1>
+						
+
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- ***** Header Text End ***** -->
+	</div>
+	
+
+	<!-- ***** Features Small Start ***** -->
+	<section class="section home-feature">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+
+					<div class="row">
+						
+						<!-- ***** Features Small Item Start *****  (EMPTY)-->
+						<div class="col-lg-2"> </div>
+						<!-- ***** Features Small Item End ***** -->
+						
+						<div class="col-lg-4 col-md-6 col-sm-6 col-8" data-scroll-reveal="enter bottom move 50px over 0.6s after 0.2s">
+							<a href="#"><div class="features-small-item">
+								<div class="icon">
+									<i><img src="Recursos/Imagenes/Consolidacion.png" alt=""></i>
+								</div>
+								<h5 class="features-title">Consolidaciones De Deudas</h5>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+									quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+								consequat.      </p>
+							</div>
+						</div></a>
+						<!-- ***** Features Small Item End ***** -->
+
+						<!-- ***** Features Small Item Start ***** -->
+						<div class="col-lg-4 col-md-6 col-sm-6 col-8" data-scroll-reveal="enter bottom move 50px over 0.6s after 0.4s">
+							<a href="#"><div class="features-small-item">
+								<div class="icon">
+									<i><img src="Recursos/Imagenes/Prestamo.png" alt=""></i>
+								</div>
+								<h5 class="features-title">Prestamos Crediticios</h5>
+								<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+									quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+								consequat.  </p>
+							</div></a>
+						</div>
+						<!-- ***** Features Small Item End ***** -->
+
+
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+	<!-- ***** Footer Start ***** -->
+	<footer>
+		<div class="container">
+			¿
+			<div class="row">
+				<div class="col-lg-12">
+					<p class="copyright">&copy; 2019 SImuladorDeCredito</p>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!-- ***** Footer End ***** -->
+
+	<!-- jQuery -->
+	<script src="Recursos/Scripts/js/jquery-2.1.0.min.js"></script>
+	<!-- Bootstrap -->
+	<script src="Recursos/Scripts/js/popper.js"></script>
+	<script src="Recursos/Scripts/js/bootstrap.min.js"></script>
+	<!-- Plugins -->
+	<script src="Recursos/Scripts/js/scrollreveal.min.js"></script>
+	<!-- Global Init -->
+	<script src="Recursos/Scripts/js/custom.js"></script>
+
+</body>
+</html>
