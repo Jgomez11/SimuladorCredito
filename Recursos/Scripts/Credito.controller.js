721c3f081fccc229c5ae2b476a51bec58279c2d6
abn1227 <37668034+abn1227@users.noreply.github.com>
2019-12-10 22:52:29 -0600

    Tablas y Chart

--- Recursos/Scripts/Credito.controller.js
+++ Recursos/Scripts/Credito.controller.js
@@ -112,21 +112,48 @@ function banpais() {
 
 function renderizarTablaAlemana(object) {
     $('#tabla').html("");
+    
+    $('#divChartCap').html("");
+    $('#divChartIxA').html("");
+
+    var labels = [0];
+    var data = {
+        capital: [object.valorPrestamo.toFixed(2)],
+        interes: [0],
+        amortizacion: [0],
+        cuota: [0]
+    };
 
     var html = '<table class="table"><thead class="thead-dark"><tr><th scope="col">Períodos</th><th scope="col">Cuotas</th><th scope="col">Interéses</th><th scope="col">Amortización</th><th scope="col">Capital Pendiente</th></tr></thead><tbody>';
     html += '<tr><th scope="row">0</th><td></td><td></td><td></td><td>' + object.valorPrestamo.toFixed(2) + '</td></tr>';
+    
     var interesTemp = 0;
     var interesAcumulado = 0;
+    
     for (let i = 0; i < object.numeroCuotas; i++) {
         interesTemp = (object.valorPrestamo) * object.interes / object.periodo;
         object.valorPrestamo -= object.amortizacion;
         interesAcumulado += interesTemp;
-        html += '<tr><th scope="row">' + (i + 1) + '</th><td>' + (object.amortizacion + interesTemp).toFixed(2) + '</td><td>' + interesTemp.toFixed(2) + '</td><td>' + object.amortizacion + '</td><td>' + object.valorPrestamo.toFixed(2) + '</td></tr>';
+        
+        labels.push(i + 1);
+        data.capital.push(object.valorPrestamo);
+        data.interes.push(interesTemp);
+        data.amortizacion.push(object.amortizacion);
+        data.cuota.push(object.amortizacion + interesTemp);
+        
+        html += '<tr><th scope="row">' + (i + 1) + '</th><td>' + (object.amortizacion + interesTemp).toFixed(2) + '</td><td>' + interesTemp.toFixed(2) + '</td><td>' + object.amortizacion.toFixed(2) + '</td><td>' + object.valorPrestamo.toFixed(2) + '</td></tr>';
     }
 
     html += '<tr><th scope="row">Totales:</th><td>' + (interesAcumulado + object.amortizacion * object.numeroCuotas).toFixed(2) + '</td><td>' + interesAcumulado.toFixed(2) + '</td><td>' + (object.amortizacion * object.numeroCuotas).toFixed(2) + '</td><td></td></tr></tbody></table>';
+    
     $("#total").val((interesAcumulado + object.amortizacion * object.numeroCuotas).toFixed(2));
+    
     $('#tabla').html(html);
+
+    $('#divChartCap').html('<canvas id="chartCapital" height="150" width="300"></canvas>');
+    $('#divChartIxA').html('<canvas id="chartInteresxAmortizacion" height="150" width="300"></canvas>');
+
+    renderizarGrafico(labels, data);
 }
 
 function renderizarTablaAmericana(object) {
@@ -138,11 +165,11 @@ function renderizarTablaAmericana(object) {
 
     for (let i = 0; i < object.numeroCuotas - 1; i++) {
         interesAcumulado += object.cuota;
-        html += '<tr><th scope="row">' + (i + 1) + '</th><td>'+ object.cuota.toFixed(2) +'</td><td>' + object.cuota.toFixed(2) + '</td><td>0.00</td><td>' + object.valorPrestamo.toFixed(2) + '</td></tr>';
+        html += '<tr><th scope="row">' + (i + 1) + '</th><td>' + object.cuota.toFixed(2) + '</td><td>' + object.cuota.toFixed(2) + '</td><td>0.00</td><td>' + object.valorPrestamo.toFixed(2) + '</td></tr>';
     }
-    
+
     interesAcumulado += object.cuota;
-    html += '<tr><th scope="row">' + object.numeroCuotas + '</th><td>'+ (object.cuota + object.valorPrestamo).toFixed(2) +'</td><td>' + object.cuota.toFixed(2) + '</td><td>'+ object.valorPrestamo.toFixed(2) +'</td><td>0.00</td></tr>';
+    html += '<tr><th scope="row">' + object.numeroCuotas + '</th><td>' + (object.cuota + object.valorPrestamo).toFixed(2) + '</td><td>' + object.cuota.toFixed(2) + '</td><td>' + object.valorPrestamo.toFixed(2) + '</td><td>0.00</td></tr>';
     html += '<tr><th scope="row">Totales:</th><td>' + (interesAcumulado + object.valorPrestamo).toFixed(2) + '</td><td>' + interesAcumulado.toFixed(2) + '</td><td>' + object.valorPrestamo.toFixed(2) + '</td><td></td></tr></tbody></table>';
     $("#total").val((interesAcumulado + object.valorPrestamo).toFixed(2));
     $('#tabla').html(html);
@@ -151,20 +178,45 @@ function renderizarTablaAmericana(object) {
 function renderizarTablaFrancesa(object) {
     $('#tabla').html("");
 
+    $('#divChartCap').html("");
+    $('#divChartIxA').html("");
+
     var html = '<table class="table"><thead class="thead-dark"><tr><th scope="col">Períodos</th><th scope="col">Cuotas</th><th scope="col">Interéses</th><th scope="col">Amortización</th><th scope="col">Capital Pendiente</th></tr></thead><tbody>';
     html += '<tr><th scope="row">0</th><td></td><td></td><td></td><td>' + object.valorPrestamo.toFixed(2) + '</td></tr>';
+    
     var interesTemp = 0;
     var interesAcumulado = 0;
+    
+    var labels = [0];
+    var data = {
+        capital: [object.valorPrestamo.toFixed(2)],
+        interes: [0],
+        amortizacion: [0],
+        cuota: [0]
+    };
+
     for (let i = 0; i < object.numeroCuotas; i++) {
         interesTemp = object.interes * object.valorPrestamo / object.periodo;
         object.valorPrestamo -= object.cuota - interesTemp;
         interesAcumulado += interesTemp;
+        
+        labels.push(i + 1);
+        data.capital.push(object.valorPrestamo);
+        data.interes.push(interesTemp);
+        data.amortizacion.push(object.cuota - interesTemp);
+        data.cuota.push(object.cuota);
+        
         html += '<tr><th scope="row">' + (i + 1) + '</th><td>' + object.cuota.toFixed(2) + '</td><td>' + interesTemp.toFixed(2) + '</td><td>' + (object.cuota - interesTemp).toFixed(2) + '</td><td>' + object.valorPrestamo.toFixed(2) + '</td></tr>';
     }
 
     html += '<tr><th scope="row">Totales:</th><td>' + (object.cuota * object.numeroCuotas).toFixed(2) + '</td><td>' + interesAcumulado.toFixed(2) + '</td><td>' + (object.cuota * object.numeroCuotas - interesAcumulado).toFixed(2) + '</td><td></td></tr></tbody></table>';
 
     $('#tabla').html(html);
+
+    $('#divChartCap').html('<canvas id="chartCapital" height="150" width="300"></canvas>');
+    $('#divChartIxA').html('<canvas id="chartInteresxAmortizacion" height="150" width="300"></canvas>');
+
+    renderizarGrafico(labels, data);
 }
 
 function renderizarTablaBanpais(object) {
@@ -199,4 +251,66 @@ function getData() {
 
 function getTipoInteres() {
     return ($("#tipoCredito").val() !== "") ? $("#tipoCredito").val() : "frances";
+}
+
+function renderizarGrafico(labels, data) {
+    var contenedorCapital = document.getElementById('chartCapital');
+    var chartCapital = new Chart(contenedorCapital, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Capital Pendiente',
+                data: data.capital,
+                backgroundColor: 'rgba(91,228,146,0.6)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Grafico de Capital Restante'
+            },
+            layout: {
+                padding: 50
+            }
+        }
+    });
+
+    var contenedorIxA = document.getElementById('chartInteresxAmortizacion');
+    var chartInteresxAmortizacion = new Chart(contenedorIxA, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [
+                {
+                    label: 'Cuota',
+                    data: data.cuota,
+                    backgroundColor: 'rgba(255, 206, 86, 0.6)',
+                    borderWidth: 1
+                },
+                {
+                    label: 'Interes',
+                    data: data.interes,
+                    backgroundColor: 'rgba(255, 99, 132, 0.6)',
+                    borderWidth: 1
+                },
+                {
+                    label: 'Amortizacion',
+                    data: data.amortizacion,
+                    backgroundColor: 'rgba(54, 162, 235, 0.6)',
+                    borderWidth: 1
+                }]
+        },
+
+        options: {
+            title: {
+                display: true,
+                text: 'Grafico de Interes contra Amortizacion',
+            },
+            layout: {
+                padding: 50
+            }
+        }
+    });
 }
